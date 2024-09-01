c888857511955f87cd2117354ca29c91b02e2cf0
rajesh196rsh <rajesh196rsh@gmail.com>
2024-09-01 14:41:14 +0530

    importing messages from constants

--- e_commerce_platform/products/constants.py
+++ e_commerce_platform/products/constants.py
@@ -1,4 +1,6 @@
+USER_CREATION_SUCCESSFUL = "User created successfully"
+INVALID_CREDENTIALS = "Invalid credentials"
 DATA_UPLOAD_SUCCESSFUL_MESSAGE = "Data uploaded successfully"
-DATA_UPLOAD_FAILURE_MESSAGE = "Data uploade failed"
+DATA_UPLOAD_FAILURE_MESSAGE = "Data upload failed"
 BULK_CREATE_BATCH_SIZE = 100
 SUMMARY_REPORT_FAILURE_MESSAGE = "Failed to create summary report"
